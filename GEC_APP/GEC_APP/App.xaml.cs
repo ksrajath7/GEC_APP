30d61c4badecb74d3d2c327206023342e8c3212a
ksrajath7 <rajathkizhiyedam@Gmail.com>
2019-09-09 13:22:26 +0530

    Add project files.

---
+++ GEC_APP/GEC_APP/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using GEC_APP.MainView;
+using GEC_APP.Feed;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GEC_APP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainViewPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
